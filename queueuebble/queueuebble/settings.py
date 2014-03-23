2f40bf63297cf29066f5478d5b881c9a99339d09
Tate Mandel <tmandel@seas.upenn.edu>
2014-03-23 14:43:24 -0400

    Started setting up database and have /hello/ showing something.

--- queueuebble/queueuebble/settings.py
+++ queueuebble/queueuebble/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'queue',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,10 @@ WSGI_APPLICATION = 'queueuebble.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'queueuebble',
+        'USER': 'queue_admin',
+        'PASSWORD': 'abc123'
     }
 }
 
--- queueuebble/queueuebble/urls.py
+++ queueuebble/queueuebble/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from queueuebble.views import hello
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'queueuebble.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^hello/$', hello,),
     url(r'^admin/', include(admin.site.urls)),
 )
