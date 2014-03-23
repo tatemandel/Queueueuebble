77b886acabb2f6bc0e5adbc63b276d2fd9e090c2
Tate Mandel <tmandel@seas.upenn.edu>
2014-03-23 18:26:37 -0400

    added first name and last name to registration

--- queueuebble/queue/forms.py
+++ queueuebble/queue/forms.py
@@ -7,7 +7,7 @@ class UserForm(forms.ModelForm):
   
   class Meta:
     model = User
-    fields = ('username', 'email', 'password')
+    fields = ('username', 'first_name', 'last_name', 'email', 'password', 'password')
 
 class UserProfileForm(forms.ModelForm):
   class Meta:
