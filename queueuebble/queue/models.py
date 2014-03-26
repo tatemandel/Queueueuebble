4bf5f914dff10db2faa7ae75a7dbde6e776ef122
Tate Mandel <tmandel@seas.upenn.edu>
2014-03-26 17:49:45 -0400

    added a queue schema to models

--- queueuebble/queue/models.py
+++ queueuebble/queue/models.py
@@ -4,8 +4,21 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
   user = models.OneToOneField(User)
 
-  website = models.URLField(blank=True)
-
   def __unicode__(self):
     return self.user.username
 
+class Queue(models.Model):
+  name = models.CharField(max_length=50)
+  owner = models.ManyToManyField(UserProfile)
+  size = models.IntegerField(default=0)
+  
+  def __unicode__(self):
+    return self.name
+
+class Node(models.Model):
+  queue = models.ForeignKey(Queue)
+  user = models.ForeignKey(UserProfile)
+  position = models.IntegerField(default=0)
+
+  def __unicode__(self):
+    return self.user
