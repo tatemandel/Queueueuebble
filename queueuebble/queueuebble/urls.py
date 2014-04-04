936bd4a45e67ef34727a63282bda4e954e4f53cb
Raul Martinez <raulde50@aol.com>
2014-04-03 21:46:58 -0400

    Can add queues

--- queueuebble/queueuebble/urls.py
+++ queueuebble/queueuebble/urls.py
@@ -17,4 +17,5 @@ urlpatterns = patterns('',
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^dashboard/$', views.dashboard, name='dashboard')
+    
 )
