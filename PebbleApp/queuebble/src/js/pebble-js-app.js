e6c8c9f8e9b0b145166e6dfe4e9bb98cfe27cc1d
Tate Mandel <tmandel@seas.upenn.edu>
2014-04-25 01:16:37 -0400

    Added cookies to the return

--- PebbleApp/queuebble/src/js/pebble-js-app.js
+++ PebbleApp/queuebble/src/js/pebble-js-app.js
@@ -17,32 +17,32 @@ Pebble.addEventListener("webviewclosed", function(e) {
 
   var options = JSON.stringify(JSON.parse(decodeURIComponent(e.response)));
   console.log("Options = " + options);
- var req = new XMLHttpRequest();
-  req.open('GET', 'http://api.openweathermap.org/data/2.1/find/city?lat=37.830310&lon=-122.270831&cnt=1', true);
-  req.onload = function(e) {
-    if (req.readyState == 4 && req.status == 200) {
-      if(req.status == 200) {
-        var response = JSON.parse(req.responseText);
-        var temperature = result.list[0].main.temp;
-        var icon = result.list[0].main.icon;
-        Pebble.sendAppMessage({ "icon":icon, "temperature":temperature + "\u00B0C"});
-      } else { console.log("Error"); }
-    }
-  }
-  req.send(null);
+ // var req = new XMLHttpRequest();
+ //  req.open('GET', 'http://api.openweathermap.org/data/2.1/find/city?lat=37.830310&lon=-122.270831&cnt=1', true);
+ //  req.onload = function(e) {
+ //    if (req.readyState == 4 && req.status == 200) {
+ //      if(req.status == 200) {
+ //        var response = JSON.parse(req.responseText);
+ //        var temperature = result.list[0].main.temp;
+ //        var icon = result.list[0].main.icon;
+ //        Pebble.sendAppMessage({ "icon":icon, "temperature":temperature + "\u00B0C"});
+ //      } else { console.log("Error"); }
+ //    }
+ //  }
+ //  req.send(null);
 
-//    var req = new XMLHttpRequest();
-//    req.open("POST", "http://54.84.161.157/login/", {
-//      "username" : options['username'],
-//      "password" : options['password']
-//    });
-//    req.onload = function(e) {
-//      if (req.status == 200) {
-//        var result =  JSON.parse(req.responseText);
-//        console.log(result);
-//      } else {
-//        console.log("FUUUUUCKKKKK");
-//      }
-//    };
-//    req.send(null);
+   var req = new XMLHttpRequest();
+   req.open("POST", "http://54.84.161.157/login/", {
+     "username" : options['username'],
+     "password" : options['password']
+   });
+   req.onload = function(e) {
+     if (req.status == 200) {
+       var result =  JSON.parse(req.responseText);
+       console.log(result);
+     } else {
+       console.log("FUUUUUCKKKKK");
+     }
+   };
+   req.send(null);
 });
