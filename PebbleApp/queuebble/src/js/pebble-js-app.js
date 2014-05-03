da50e0fb531c83b271c34ea6eacbd1735a964835
Tate Mandel <tmandel@seas.upenn.edu>
2014-05-03 16:04:13 -0400

    pebble app gets the username

--- PebbleApp/queuebble/src/js/pebble-js-app.js
+++ PebbleApp/queuebble/src/js/pebble-js-app.js
@@ -13,24 +13,23 @@ Pebble.addEventListener("showConfiguration",
 
 Pebble.addEventListener("webviewclosed", function(e) {
   console.log("Configuration window closed");
-
   var options = JSON.parse(decodeURIComponent(e.response));
-  console.log("Options = " + JSON.stringify(options));
   console.log(options['username'])
-  console.log(options['password'])
 
-  var http = new XMLHttpRequest();
-  var params = "username=" + options['username'] + "&password=" + options['password'];
-  http.open("POST", "HTTP://54.84.161.157/pebble_get_queues/", true);
 
-  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  http.setRequestHeader("Content-length", params.length);
-  http.setRequestHeader("Connection", "close");
 
-  http.onload = function() {
-    if (http.status == 200) {
-      console.log(http.responseText);
-    }
-  }
-  http.send(params);
+  // var http = new XMLHttpRequest();
+  // var params = "username=" + options['username'] + "&password=" + options['password'];
+  // http.open("POST", "HTTP://54.84.161.157/pebble_get_queues/", true);
+
+  // http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  // http.setRequestHeader("Content-length", params.length);
+  // http.setRequestHeader("Connection", "close");
+
+  // http.onload = function() {
+  //   if (http.status == 200) {
+  //     console.log(http.responseText);
+  //   }
+  // }
+  // http.send(params);
 });
